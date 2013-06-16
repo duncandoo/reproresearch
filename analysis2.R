79cc64f0afe80793caa5db54ecd19cf38ce06aa1
Duncan McPherson <duncan@mcpherson.org.uk>
2013-06-16 11:53:59 +0100

    amend df name to reflect output of clean.r

--- analysis2.R
+++ analysis2.R
@@ -1,7 +1,7 @@
 library(xtable)
 
 # Output counts of the categorical data
-xout <- xtable(table(catsData$Animal, catsData$Color),
+xout <- xtable(table(animalData$Animal, animalData$Color),
                caption="Pets and Color.\\label{tab:petColor}")
 align(xout) <- "|l|l|l|l|l|"
 print(xout, file="tex/petColor.tex")
